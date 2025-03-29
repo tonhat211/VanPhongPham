085556a5821ffb7d1ee5f980cad1560377736acd
PhanTTDuong <21130331@st.hcmuaf.edu.vn>
2025-03-29 18:03:26 +0700

    homepage

--- stationery-app/src/component/footer/footer.js
+++ stationery-app/src/component/footer/footer.js
@@ -2,75 +2,64 @@ import {memo} from "react"
 import Icon from '@mdi/react';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBInput, MDBIcon, MDBBtn }  from 'mdb-react-ui-kit';
+import logo from '../../resource/logo.webp';
 function Footer(){
     return (
-        
+
         <MDBFooter   className='text-center  text-lg-start text-muted'>
-          <section className='' style={{ backgroundColor: "rgba(38, 89, 243, 1)" }}>
+          <section className='' style={{ backgroundColor: "#ffffff" }}>
             <MDBContainer className='text-center text-md-start mt-5 p-2'>
               <MDBRow className='mt-3'>
-                <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4 text-light'>
-                  <h6 className='text-uppercase fw-bold mb-4'>
-                    <MDBIcon color='secondary' icon='gem' className='me-3 text-light' />
-                    Thienlong.vn
+                <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4'>
+                  <h6 className='fw-bold mb-3'>
+                    <img className="img-fluid" src={logo} alt="Thiên Long"/>
                   </h6>
-                  <p >
-                  Công ty Cổ Phần Tập Đoàn Thiên Long
-                GPĐKKD số 0301464830 do Sở KHĐT TP. Hồ Chí Minh cấp ngày 14/03/2005
+                  <p style={{fontSize: '14px', color: '#666', lineHeight: '1.6'}}>
+                    <strong style={{fontSize: '14px', color: '#000', fontWeight: 'bold'}}>
+                      Công ty Cổ Phần Tập Đoàn Thiên Long
+                    </strong>
+                    <br/>
+                    GPĐKKD số 0301464830 do Sở KHĐT TP. Hồ Chí Minh cấp ngày 14/03/2005.
+                  </p>
+
+                </MDBCol>
+
+                <MDBCol md='3' lg='3' xl='3' className='mx-auto mb-4'>
+                  <h6 className='fw-bold mb-3' style={{color: '#17479d'}}>ĐỊA CHỈ CÔNG TY</h6>
+                  <p style={{fontSize: '14px', color: '#666', lineHeight: '1.6'}}>
+                    <strong style={{ fontSize: '14px', color: '#000', fontWeight: 'bold' }} >Head Office:</strong> Tầng 10, Sofic Tower, Số 10 Đường Mai Chí Thọ, Phường Thủ Thiêm, TP Thủ Đức, TP Hồ Chí Minh, Việt Nam.
+                  </p>
+                  <p style={{ fontSize: '14px', color: '#666', lineHeight: '1.6' }}>
+                    <strong style={{ fontSize: '14px', color: '#000', fontWeight: 'bold' }}>Miền Bắc:</strong> Số 38, đường Gamuda Gardens 2-5, KĐT C2 - Gamuda Gardens, Hoàng Mai, Hà Nội.
                   </p>
                 </MDBCol>
-    
-                <MDBCol md='2' lg='2' xl='2' className='mx-auto mb-4 text-light'>
-                  <h6 className='text-uppercase fw-bold mb-4'>ĐỊA CHỈ CÔNG TY</h6>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                    Head Office: Tầng 10, Sofic Tower, Số 10 Đường Mai Chí Thọ, Phường Thủ Thiêm, Thành Phố Thủ Đức, Thành Phố Hồ Chí Minh, Việt Nam
-                    </a>
-                  </p>
+
+                <MDBCol md='3' lg='2' xl='2' className='mx-auto mb-4'>
+                  <h6 className='fw-bold mb-3' style={{ color: '#17479d' }}>HỖ TRỢ KHÁCH HÀNG</h6>
+                  <p style={{ fontSize: '14px', color: '#000', fontWeight: 'bold' }}>Hotline: 1900 866 819</p>
+                  <p style={{ fontSize: '14px', color: '#000', fontWeight: 'bold'}}>Thứ 2 - Thứ 6 (8h - 17h)</p>
+                  <p style={{ fontSize: '14px', color: '#000', fontWeight: 'bold' }}>salesonline@thienlongvn.com</p>
+                  <p style={{ fontSize: '14px', color: '#666' }}>Hướng dẫn mua hàng</p>
+                  <p style={{ fontSize: '14px', color: '#666' }}>Chính sách giao hàng</p>
+                  <p style={{ fontSize: '14px', color: '#666' }}>Chính sách đổi trả & hoàn tiền</p>
                 </MDBCol>
-    
-                <MDBCol md='3' lg='2' xl='2' className='mx-auto mb-4 text-light'>
-                  <h6 className='text-uppercase fw-bold mb-4'>HỖ TRỢ KHÁCH HÀNG
-                    Hotline: 1900 866 819
-                    </h6>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                    Hướng dẫn mua hàng
-                    </a>
-                  </p>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                    Chính sách mua hàng
-                    </a>
-                  </p>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                      Sản phẩm giá tốt
-                    </a>
-                  </p>
+
+                <MDBCol md='3' lg='3' xl='3' className='mx-auto mb-4'>
+                  <h6 className='fw-bold mb-3' style={{ color: '#17479d' }}>VỀ THIENLONG.VN</h6>
+                  <p style={{ fontSize: '14px', color: '#666' }}>Giới thiệu</p>
+                  <p style={{ fontSize: '14px', color: '#666' }}>Dịch vụ in ấn quảng cáo</p>
+                  <p style={{ fontSize: '14px', color: '#666' }}>Chính sách bảo mật chung</p>
+                  <p style={{ fontSize: '14px', color: '#666' }}>Chính sách bảo mật thông tin cá nhân</p>
+                  <p style={{ fontSize: '14px', color: '#666' }}>Thông tin liên hệ</p>
                 </MDBCol>
-    
-                <MDBCol md='4' lg='3' xl='3' className='mx-auto mb-md-0 mb-4 text-light'>
-                  <h6 className='text-uppercase fw-bold mb-4'>VỀ THIENLONG.VN</h6>
-                  <p>
-                    Giới thiệu
-                  </p>
-                  <p>
-                    Chính sách bảo mật
-                  </p>
-                  <p>
-                    Thông tin liên hệ
-                  </p>
-                </MDBCol>
+
               </MDBRow>
             </MDBContainer>
           </section>
-    
-          <div className='text-center p-2 text-light' style={{ backgroundColor: "rgba(0, 60, 209, 1)" }}>
-            © 2021 Copyright:
-            <a className='text-reset fw-bold' href='/home'>
-              Công ty Thiên Long
-            </a>
+
+          <div className='text-center p-2' style={{ backgroundColor: "#e8efff", color: '#3b71ca' }}>
+            © 2021 Copyright - Bản quyền thuộc Tập đoàn Thiên Long
+
           </div>
         </MDBFooter>
       );
