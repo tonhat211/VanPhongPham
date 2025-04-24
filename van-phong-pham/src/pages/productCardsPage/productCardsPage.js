f5c066ae87107319f5b44b35fb6643c7322bad29
PhanTTDuong <21130331@st.hcmuaf.edu.vn>
2025-04-24 18:06:42 +0700

    carouselCardsProducts

--- van-phong-pham/src/pages/productCardsPage/productCardsPage.js
+++ van-phong-pham/src/pages/productCardsPage/productCardsPage.js
@@ -5,6 +5,7 @@ import './productCardsPage.scss';
 import { formatPrices, formatPercentage } from '~/utils/common';
 import LocalShippingRoundedIcon from '@mui/icons-material/LocalShippingRounded';
 import TickDiscount from '~/components/Layouts/components/ProductDetail/discountsticket/TickDiscount';
+import CarouselCards from '~/components/Layouts/components/ProductDetail/carouselCards/CarouselCards';
 function ProductCardsPage() {
     const navigate = useNavigate();
     const [cartItems, setCartItems] = useState(cartItemsData);
@@ -43,71 +44,81 @@ function ProductCardsPage() {
 
     return (
         <section className="cart-container">
-            <h3 className="cart-title">Giỏ hàng</h3>
-            <div className="cart-wrapper">
-                <div className="cart-left">
-                    <div className="cart-items">
-                        {cartItems.length > 0 ? (
-                            cartItems.map(item => (
-                                <div className="cart-item" key={item.sid}>
-                                    <img className="item-image" src={item.images[0].url} alt={item.title} />
-                                    <div className="item-details">
-                                        <p className="item-title">{item.title}</p>
-                                        <p className="item-brand">{item.brand}</p>
-                                    </div>
+            <div className="cart-info">
+                <h3 className="cart-title">Giỏ hàng</h3>
+                <div className="cart-wrapper">
+                    <div className="cart-left">
+                        <div className="cart-items">
+                            {cartItems.length > 0 ? (
+                                cartItems.map(item => (
+                                    <div className="cart-item" key={item.sid}>
+                                        <img className="item-image" src={item.images[0].url} alt={item.title} />
+                                        <div className="item-details">
+                                            <p className="item-title">{item.title}</p>
+                                            <p className="item-brand">{item.brand}</p>
+                                        </div>
 
-                                    <div className="item-price">
-                                        {formatPrices(item.price)}
-                                        {item.originalPrice && (
-                                            <>
-                                                <del>{formatPrices(item.originalPrice)}</del>
-                                                <span className="discount">
+                                        <div className="item-price">
+                                            {formatPrices(item.price)}
+                                            {item.originalPrice && (
+                                                <>
+                                                    <del>{formatPrices(item.originalPrice)}</del>
+                                                    <span className="discount">
                 -{formatPercentage(item.discountPercent)}
               </span>
-                                            </>
-                                        )}
-                                    </div>
+                                                </>
+                                            )}
+                                        </div>
 
-                                    <div className="item-quantity">
-                                        <button onClick={() => handleDecrement(item.sid)}>-</button>
-                                        <input type="number" value={item.quantity} min="1" readOnly />
-                                        <button onClick={() => handleIncrement(item.sid)}>+</button>
-                                    </div>
+                                        <div className="item-quantity">
+                                            <button onClick={() => handleDecrement(item.sid)}>-</button>
+                                            <input type="number" value={item.quantity} min="1" readOnly />
+                                            <button onClick={() => handleIncrement(item.sid)}>+</button>
+                                        </div>
 
-                                    <button className="item-remove" onClick={() => handleRemove(item.sid)}>×</button>
+                                        <button className="item-remove" onClick={() => handleRemove(item.sid)}>×
+                                        </button>
+                                    </div>
+                                ))
+                            ) : (
+                                <div className="cart-empty">
+                                    <p>Bạn chưa có sản phẩm nào trong giỏ hàng - quay về <a href="#" title="Trang Chủ"
+                                                                                            className="link-homepage"> Trang
+                                        Chủ</a> để
+                                        mua hàng</p>
                                 </div>
-                            ))
-                        ) : (
-                            <div className="cart-empty">
-                                <p>Bạn chưa có sản phẩm nào trong giỏ hàng - quay về <a href="#" title="Trang Chủ" className="link-homepage"> Trang Chủ</a> để
-                                    mua hàng</p>
-                            </div>
-                        )}
+                            )}
+                        </div>
+
+                        <div className="cart-note">
+                            <p className="shipping-note">
+                                <LocalShippingRoundedIcon className="icon-shipping"></LocalShippingRoundedIcon>
+                                Miễn phí vận chuyển cho đơn hàng từ 100,000₫</p>
+                            <label className="company-invoice">
+                                <input type="checkbox" />
+                                Xuất hoá đơn công ty
+                            </label>
+                            <textarea placeholder="Ghi chú đơn hàng"></textarea>
+                        </div>
+
                     </div>
 
-                    <div className="cart-note">
-                        <p className="shipping-note">
-                            <LocalShippingRoundedIcon  className="icon-shipping"></LocalShippingRoundedIcon>
-                            Miễn phí vận chuyển cho đơn hàng từ 100,000₫</p>
-                        <label className="company-invoice">
-                            <input type="checkbox" />
-                            Xuất hoá đơn công ty
-                        </label>
-                        <textarea placeholder="Ghi chú đơn hàng"></textarea>
+                    <div className="cart-right">
+                        <div className="total-cart-container">
+                            <p className="total-title">Tổng tiền</p>
+                            <spanp className="total-price">{formatPrices(total)}</spanp>
+                        </div>
+                        <button className="checkout-button" onClick={handleCheckout}>
+                            Tiến hành đặt hàng
+                        </button>
+                        <TickDiscount />
                     </div>
 
                 </div>
+            </div>
 
-                <div className="cart-right">
-                    <div className="total-cart-container">
-                        <p className="total-title">Tổng tiền</p>
-                        <spanp className="total-price">{formatPrices(total)}</spanp>
-                    </div>
-                    <button className="checkout-button" onClick={handleCheckout}>
-                        Tiến hành đặt hàng
-                    </button>
-                    <TickDiscount/>
-                </div>
+            <div className="carousel-section">
+                <CarouselCards />
             </div>
         </section>
     );
